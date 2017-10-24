8352469998b95a9c1e40ec5999b21dd610a88cad
nhatduy <nhatduy1tn@gmail.com>
2017-10-24 14:19:08 +0700

    redirect url

--- public/javascripts/configs.js
+++ public/javascripts/configs.js
@@ -1,5 +1,5 @@
 const INSTAGRAM_API_CLIENT_ID = 'ae1b2ed6061748e69b61f413e8dab8b4';
-const INSTAGRAM_REDIRECT_URI = 'https://cnm-btcn02-1412081.herokuapp.com/';
+const INSTAGRAM_REDIRECT_URI = 'https://cnm-btcn02-1412081.herokuapp.com';
 // const INSTAGRAM_REDIRECT_URI = 'http://localhost:3000';
 const GG_API_KEY = 'AIzaSyCma6bUzscck3QFJcmoQILck898CRUVg2k';
 
