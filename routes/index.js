a1f5ae7da960fd91ee706845466e7e63efe309f0
nhatduy <nhatduy1tn@gmail.com>
2017-10-24 14:28:11 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -38,9 +38,10 @@ router.get('/get-instagram-location-media', function(req, res, next) {
     let location_id = req.query.location_id;
     let url = `https://www.instagram.com/explore/locations/${location_id}/?__a=1`;
 
-    send_https_GET_request(url, function (response) {
-        res.json(response);
-    });
+    // send_https_GET_request(url, function (response) {
+    //     res.json(response);
+    // });
+    res.end("123");
 });
 
 module.exports = router;
